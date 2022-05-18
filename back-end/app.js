83eb88f98fe6f9e82340cbe0fb6b862764581d4f
mahatabhossain <mahatabhossain28@gmail.com>
2022-05-18 10:13:11 +0530

    MVC structure added

--- back-end/app.js
+++ back-end/app.js
@@ -1,31 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
-
+// const path = require("path");
 const app = express();
-const port = 8000;
 
+//"cors()" enables you to access a resource from a different origin.
 app.use(cors());
-app.use(express.json()); // allows us to accept the data in json format.
 
-// DATABASE  CONNECTION ESTABLISHMENT
-mongoose.connect(
-  "mongodb+srv://myNewDatabase:Mahatab123@cluster0.zhoox.mongodb.net/bookstoredb?retryWrites=true&w=majority",
-
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("database connected successfully");
-});
+//"express.json()" allows us to accept the data in json format.
+app.use(express.json());
 
 //ROUTER
 app.use("/", require("./router"));
 
-app.listen(port, () => console.log(`Server running on ${port} port`));
+module.exports = app;
